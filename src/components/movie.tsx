3cdabc9b3a102305de7633985c24d1c897057566
ParkKiHun <miy1205@naver.com>
2024-04-22 17:12:18 +0900

    commit

---
+++ src/components/movie.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "styles/movie.module.css";
+
+interface IMovieProps {
+  id: string;
+  poster_path: string;
+  title: string;
+}
+
+export default function Movie({ id, poster_path, title }: IMovieProps) {
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
